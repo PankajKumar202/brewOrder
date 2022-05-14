f0ccafc4058caf1e972fcf95643563e19105ede8
PankajKumar202 <parya209@gmail.com>
2022-05-14 20:18:39 +0530

    final

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'https://brewmusefspk.netlify.app/callback';
+    params['CALLBACK_URL'] = 'https://brewdeliver.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
